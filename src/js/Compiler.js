10cc021a949db6e07f6901b0052398a8ec7a5569
Je <Je@Je.local>
2019-02-06 16:14:01 +0900

    :hammer: language 추가 할수 있도록 변경.

---
+++ src/js/Compiler.js
@@ -0,0 +1,39 @@
+import Quiz from './Quiz';
+import template from "./templates";
+
+window.onload = init;
+
+function init() {
+    appendOption();
+    const elQuiz = document.getElementById('quiz');
+    const quiz = new Quiz('Quiz1');
+    elQuiz.innerHTML = quiz.content;
+    elQuiz.setAttribute('result', quiz.result);
+}
+
+function appendOption() {
+    const elSelectBox = document.getElementById('selectBox');
+    let str = '';
+    Object.keys(template).forEach(quiz => {
+        str += `<option value="${quiz}">${quiz}</option>`;
+    });
+    elSelectBox.innerHTML = str;
+
+    elSelectBox.addEventListener('change', changeQuiz);
+}
+
+function changeQuiz() {
+    const elSelectBox = document.getElementById('selectBox');
+    const elQuiz = document.getElementById('quiz');
+    let value = elSelectBox.value;
+    let quiz = new Quiz(value);
+    elQuiz.innerHTML = quiz.content;
+    elQuiz.setAttribute('result', quiz.result);
+}
+
+function changeLanguage() {
+
+}
+
+
+export default init;
